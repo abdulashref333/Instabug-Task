ceefb5d928b80d34b837c81159a7c733e896050c
Abdulrahman Ashraf <abdelrahman.ashraf@espace.com.eg>
2024-03-23 14:29:53 +0200

    Add Application controller

---
+++ app/controllers/api/v1/applications_controller.rb
@@ -0,0 +1,46 @@
+module Api
+ module V1
+    class ApplicationsController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      # POST /api/v1/applications
+      def create
+        @application = Application.new(application_params)
+        @application.token = SecureRandom.uuid
+        if @application.save
+          render json: @application, status: :created
+        else
+          render json: @application.errors, status: :unprocessable_entity
+        end
+      end
+
+      # GET /api/v1/applications/:token
+      def show
+        @application = Application.find_by(token: params[:token])
+        if @application
+          render json: @application
+        else
+          render json: { error: 'Application not found' }, status: :not_found
+        end
+      end
+
+      def update
+        @application = Application.find_by(token: params[:token])
+        if @application
+          if @application.update(application_params)
+            render json: @application
+          else
+            render json: @application.errors, status: :unprocessable_entity
+          end
+        else
+          render json: { error: 'Application not found' }, status: :not_found
+        end
+      end
+
+      private
+
+      def application_params
+        params.require(:application).permit(:name)
+      end
+    end
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      # Applications routes
+      post '/applications', to: 'applications#create'
+      get '/applications/:token', to: 'applications#show', as: :application
+      patch '/applications/:token', to: 'applications#update'
+
+      # Chats routes
+      post '/applications/:token/chats', to: 'chats#create'
+      get '/applications/:token/chats', to: 'chats#index'
+      get '/applications/:token/chats/:number', to: 'chats#show', as: :chat
+
+      # Messages routes
+      post '/applications/:token/chats/:number/messages', to: 'messages#create'
+      get '/applications/:token/chats/:number/messages', to: 'messages#index'
+      get '/applications/:token/chats/:number/messages/:number', to: 'messages#show', as: :message
+      patch '/applications/:token/chats/:number/messages/:number', to: 'messages#update'
+    end
+  end
 end
